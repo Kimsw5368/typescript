b75f4f01e22cd5c51def52975065aa0653a9be8a
김성욱 <70447614+Kimsw5368@users.noreply.github.com>
2021-01-19 17:57:44 +0900

    프로젝트 생성 및 관리

---
+++ ch02-2/src/index.ts
@@ -0,0 +1,9 @@
+import IPerson from './Person'
+import Person from './Person'
+import Chance from 'chance'
+import * as R from 'ramda'
+
+const chance = new Chance()
+let person: IPerson[] = R.range(0, 2)
+  .map((n: number) => new Person(chance.name(), chance.age()))
+console.log(person)
