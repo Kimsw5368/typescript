67f76575686837fbf88ed80a7b4a86f400528829
김성욱 <70447614+Kimsw5368@users.noreply.github.com>
2021-01-20 16:11:54 +0900

    Delete IPerson.ts

--- IPerson.ts
+++
@@ -1,9 +0,0 @@
-interface IPerson1 {
-  name: string // 필수 속성
-  age: number // 필수 속성
-  etc?: boolean // 선택 속성
-}
-let good1: IPerson1 = {name: 'Jack', age: 32}
-let good2: IPerson1 = {name: 'Jack', age: 32, etc: true}
-
-console.log(good2)
